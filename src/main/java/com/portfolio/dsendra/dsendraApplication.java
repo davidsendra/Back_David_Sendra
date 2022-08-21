beb7e485ccc5d80bd33619b5cf83432d0ea41e64
davidsendra <97367760+davidsendra@users.noreply.github.com>
2022-08-21 18:53:50 -0300

    armado de base de datos

---
+++ src/main/java/com/portfolio/dsendra/dsendraApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dsendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class dsendraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(dsendraApplication.class, args);
+	}
+
+}
